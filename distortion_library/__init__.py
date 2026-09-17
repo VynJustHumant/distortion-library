1c35e95d44c64b8858bfa7bcebe8297f42725ed7
JustHumant <yasudahlahcuy@gmail.com>
2026-09-18 00:56:29 +0800

    Update __init__.py

--- distortion_library/__init__.py
+++ distortion_library/__init__.py
@@ -1,7 +1,21 @@
 """
 Distortion Library
+==================
+Differentiable image augmentation for computer vision.
 """
-from .blur_atmospheric_turbulence_v1 import blur_atmospheric_turbulence_v1
 
-__version__ = "0.1.0"
-__all__ = ["blur_atmospheric_turbulence_v1"]
+from .blur_atmospheric_turbulence_v1 import blur_atmospheric_turbulence_v1
+from .blur_frc_optical_flow_v1 import blur_frc_optical_flow_v1
+
+try:
+    from .motion_blur_linear import DISTORTION_REGISTRY
+except ImportError:
+    from .blur_atmospheric_turbulence_v1 import DISTORTION_REGISTRY
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "DISTORTION_REGISTRY",
+    "blur_atmospheric_turbulence_v1",
+    "blur_frc_optical_flow_v1",
+]
